1377205f36bc2a59e437ad214cc9ade725146a55
mintball <132421852+Siriphat101@users.noreply.github.com>
2023-06-20 12:14:11 +0700

    delete R
    delete R from mainApp/urls.py and views.py

--- ToDoList/mainApp/urls.py
+++ ToDoList/mainApp/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     
-    path('',ListTodo.as_view()),
+    # path('',ListTodo.as_view()),
     path('create',CreateTodo.as_view()),
     path('update/<int:pk>/',DetailTodo.as_view()),
     path('delete/<int:pk>',DeleteTodo.as_view()),
--- ToDoList/mainApp/views.py
+++ ToDoList/mainApp/views.py
@@ -1,24 +1,3 @@
-# from django.shortcuts import render
-# from rest_framework import generics
-# from .serializers import *
-# from .models import *
-# # Create your views here.
-# class CreateTodo(generics.CreateAPIView):
-#     queryset = ToDo.objects.all()
-#     serializer_class = ToDoSerializer 
-    
-# class ListTodo(generics.ListAPIView):
-#     queryset = ToDo.objects.all()
-#     serializer_class = ToDoSerializer
-    
-# class DetailTodo(generics.RetrieveUpdateAPIView):
-#     queryset = ToDo.objects.all()
-#     serializer_class = ToDoSerializer
-    
-# class DeleteTodo(generics.DeleteAPIView):
-#     queryset = ToDo.objects.all()
-#     serializer_class = ToDoSerializer
-
 from rest_framework import generics
 from .serializers import ToDoSerializer
 from .models import ToDo
@@ -29,9 +8,9 @@ class CreateTodo(generics.CreateAPIView):
     serializer_class = ToDoSerializer
     
 
-class ListTodo(generics.ListAPIView):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
+# class ListTodo(generics.ListAPIView):
+#     queryset = ToDo.objects.all()
+#     serializer_class = ToDoSerializer
 
 class DetailTodo(generics.RetrieveUpdateAPIView):
     queryset = ToDo.objects.all()
